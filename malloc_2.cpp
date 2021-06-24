aefa5fe7b0d9b409d49b2ddce20c882585a81244
Nativ Maor <nativ.maor@gmail.com>
2021-06-24 18:52:37 +0000

    small additions

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -4,20 +4,24 @@
 class MetaData{
     size_t  _size;
     bool _is_free;
+    void* _data_block;
     MetaData* _next;
     MetaData* _prev;
     public:
     MetaData(size_t size ,bool is_free, MetaData* last_metadata):_size(size),_is_free(is_free),_prev(last_metadata),_next(nullptr){
-        
     }
     size_t getSize(){
         return _size;
     }
-};
-class BlockData{
-    MetaData* _metadata;
+    bool isFree(){
+        return _is_free;
+    }
+    bool setFree(bool new_free_status){
+        _is_free=new_free_status;        
+    }
 
-}
+
+};
 
 
 MetaData *heap_bottom=nullptr;
@@ -35,18 +39,30 @@ void* sbrk_wrap(size_t size){
 }
 
 void* smalloc(size_t size){
+    void* sbrk_res;
     if (heap_bottom){
-        void* sbrk_res= sbrk_wrap(sizeof(MetaData)+size);
+        sbrk_res= sbrk_wrap(sizeof(MetaData)+size);
         if (sbrk_res==NULL){
             return NULL;
         }
         heap_bottom=(MetaData*)sbrk_res;
         return heap_bottom+sizeof(MetaData);
     }
-    
+    MetaData* itt= heap_bottom;
+    while (itt){
+        if (itt->isFree()&& itt->getSize()>=size){
+            sbrk_res= sbrk_wrap(sizeof(MetaData)+size);
+            if (sbrk_res==NULL){
+                return NULL;
+            }
+            heap_bottom=(MetaData*)sbrk_res;
+            return heap_bottom+sizeof(MetaData);
+        }        
+    }
 }
-void* scalloc(size_t num, size_t size){
 
+void* scalloc(size_t num, size_t size){
+    void* smalloc_res=smalloc(size);
 }
 void* sfree(void* p){
 
